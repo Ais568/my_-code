fd63dd01881c81fa389fe8dc894d87a6b9ffa2bc
Ais568 <aiswaryapadavala@gmail.com>
2026-08-30 19:24:27 +0530

    Add files via upload

---
+++ all.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+//int Cube(int n)
+//{   int ans;
+//    ans=n*n*n;
+//    return ans;
+//}
+
+//int main()
+//{
+//    int a;
+//    cout<<"Enter your number:";
+//    cin>>a;
+    
+ //   cout<<Cube(a)<<endl;
+	
+
+//}
+
+//void Swap(int &a,int &b)
+//{
+ //   int c;
+ //   c=a;
+ ////   a=b;
+ //   b=c;
+//}
+//int main()
+//{
+ //   int a,b;
+  //  cin>>a>>b;
+  //  Swap(a,b);
+  ////  cout<<a<<" "<<b<<" ";
+//}
+
+//void Swap(int &a,int &b,int &c)
+//{
+//    int d;
+////    d=a;
+//    a=b;
+//    b=c;
+//    c=d;
+//}
+//int main()
+//{
+ ////   int a=1;int b=2;int c=3;
+ //   Swap(a,b,c);
+  //  cout<<a<<" "<<b<<" "<
+//  void fun(int n)
+//{   
+ //   int i;
+ //   for(i=1;i<=n;i++)
+ //   {
+  ///      cout<<"Hello coder army";
+//}
+///}
+////int main()
+//{
+ //   int n=5;
+ //   fun(n);
+    
+//}
+
+
+}
