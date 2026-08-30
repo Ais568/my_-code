625f7f21f144f76a53db6f323d64173fff94c58e
Ais568 <aiswaryapadavala@gmail.com>
2026-08-30 14:28:11 +0530

    Add initial C++ program structure

---
+++ practice_cpp.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ //   int row;
+  //  int col;
+//	for(row=1;row<=5;row=row+1)
+//	{
+	 //  for(col=5;col>=row;col=col-1)
+	 //  {
+	  //     cout<<col<<" ";
+	  // }
+	  //   cout<<endl;
+	   
+//	}
+    
+	 
+
+}
