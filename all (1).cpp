b9000a1ebd89afd7726844cb531f4fd43ef924d0
Ais568 <aiswaryapadavala@gmail.com>
2026-08-31 21:39:07 +0530

    Add files via upload

---
+++ all (1).cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+//char convert(char name)
+//{
+ //   char ans;
+ //   ans=name-'a'+'A';
+ //   return ans;
+//}
+
+//int main()
+//{
+//	char name;
+//	cin>>name;
+//	cout<<convert(name)<<endl;
+
+//}
+//int countdigit(int n)
+//{
+//    int count=0;
+ //   while(n)
+ //   {
+ //       count++;
+ //       n/=10;
+        
+ //   }
+ //     return count;
+//}
+
+//bool Armstrong(int num,int digit)
+//{
+  //  int n=num,ans=0,rem;
+  //  while(n)
+  //  {
+        
+    //    rem=n%10;
+     //   n/=10;
+     //   ans=ans+round(pow(rem,digit));//
+  //  }
+    // if(ans==num)
+   //  return 1;
+    // else
+   //  return 0;
+//}
+//int main()
+//{
+  //  int num;
+//cin>>num;
+  //  int digit=countdigit(num);
+   // cout<<Armstrong(num,digit);
+//}
+
