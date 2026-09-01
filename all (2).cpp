b3c64a90bf15c6f46742b7a996feadd0ef49ca0e
Ais568 <aiswaryapadavala@gmail.com>
2026-09-01 19:28:34 +0530

    Add files via upload

---
+++ all (2).cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+//	int arr[5]={1,2,3,4,5};
+//	cout<<arr[0];
+
+int arr[5];
+
+   for(int i=0;i<5;i++)
+   cin>>arr[i];
+   
+    for(int i=0;i<5;i++)
+    cout<<arr[i]<<" ";
+    
+    return 0;
+  
+   
+
+//  int arr[5]={1,2,3,4,5};
+//  int ans=INT_MAX;
+  
+//  for( int i=0;i<5;i++)
+//  {
+ //     if(arr[i]<ans)
+ //     ans=arr[i];
+ // }
+ //  cout<<ans;
+ 
+// int arr[5]={1,2,3,4,5};
+// int ans=INT_MIN;
+// int i;
+// for(i=0;i<5;i++)
+// {
+ ///    if(arr[i]>ans)
+ //    ans=arr[i];
+/// }
+//   cout<<ans;
+ // int arr[5]={1,2,3,4,5};
+//  bool a[0];
+ // int
+ // char
+ // cout<<sizeof(a[0]);
+ 
+// int size();
+// cin>>size;
+ 
+// int arr[100];
+// int i;
+// for(i=0;i<size;i++)
+// {
+ //    cin>>arr[i];
+// }
+// int i;
+// for(i=0;i<6;i++)
+// {
+ //    cout<<arr[i];
+ //}
+ 
+// int arr[5]={1,2,3,4,5};
+// cin>>arr[5];
+// int sum=0;
+// int i;
+// for(i=0;i<5;i++)
+// {
+ //    cin>>arr[i];
+     
+ //    sum=sum+arr[i];
+     
+// }
+ // cout<<sum;
+   
+//   return 0;
+
+//char arr[26];
+
+////int i;
+//for(i=0;i<26;i++)
+//{
+ //   arr[i]='a'+i;
+ //   cout<<arr[i];
+//}
+
+//int arr[5]={1,2,3,4,5};
+//int ans1=INT_MIN;
+//int ans2=INT_MIN;
+//for(int i=0;i<5;i++)
+//{
+//    if(arr[i]>ans1)
+//    {
+//        ans2=ans1;
+ //       ans1=arr[i];
+ //   }
+ //   else if(arr[i]>ans2 && arr[i]!=ans1)
+  //  {
+  //      ans2=arr[i];
+  //  }
+//}
+//  cout<<ans2;
+//  return 0;
+
+//int arr[5]={1,2,3,4,5};
+//int ans1=INT_MAX;
+//int ans2=INT_MAX;
+//int ans3=INT_MAX;
+//int i;
+//for(i=0;i<5;i++)
+//{
+  //  if(arr[i]<ans1)
+    //{
+//    ans3=ans2;
+  //  ans2=ans1;
+    //ans1=arr[i];
+    //}else if(arr[i]<ans2)
+//    {//
+  //      ans3=ans2;
+    //    ans2=arr[i];
+//    }else if(arr[i]<ans3)
+  //  {
+    //    ans3=arr[i];
+    //}
+      //cout<<ans3;
+     // return 0;
+}
+
+
+
+ 
+ 
+ 
+ 
+}
